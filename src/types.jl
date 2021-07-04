754b8e798eaeb15996fdb8b5bd025867150e623f
Andrew Hynes <andrewjhynes@gmail.com>
2021-07-04 17:32:49 -0400

    feat: enforce that Line fields have same length

--- src/types.jl
+++ src/types.jl
@@ -1,4 +1,13 @@
 struct Line
     point::AbstractVector
     direction::AbstractVector
+
+    function Line(point::AbstractVector, direction::AbstractVector)
+
+        if length(point) != length(direction)
+            throw(ArgumentError("The point and direction must have the same length."))
+        end
+
+        new(point, direction)
+    end
 end
--- test/types.jl
+++ test/types.jl
@@ -9,4 +9,9 @@
     @test_throws MethodError Line([0, 0], 1)
     @test_throws MethodError Line([0 0; 0 0], [1, 0])
     @test_throws MethodError Line([0, 0], [1 0; 0 0])
+
+    message = "The point and direction must have the same length."
+    @test_throws ArgumentError(message) Line([0], [1, 0])
+    @test_throws ArgumentError(message) Line([0, 0], [1, 0, 0])
+    @test_throws ArgumentError(message) Line([0, 0, 0], [1, 0])
 end
