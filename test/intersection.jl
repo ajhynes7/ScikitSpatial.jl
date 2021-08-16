a9044cecc520ff979f11b306879ac5640fc4bc97
Andrew Hynes <andrewjhynes@gmail.com>
2021-08-16 22:17:48 +0200

    feat!: Add intersection method for sphere and plane
    Also change order of inputs for `intersect` methods.
    
    `intersect(plane, line)`
    `intersect(circle, line)`
    `intersect(sphere, line)`
    
    felt more intuitive to me, and this order aligns with the Python methods `Plane.intersect_line`, `Circle.intersect_line`, and `Sphere.intersect_line` in `scikit-spatial`.

--- test/intersection.jl
+++ test/intersection.jl
@@ -1,4 +1,4 @@
-@testset "Line-line intersection" begin
+@testset "Intersection of two lines" begin
 
     line_a = Line([0, 0], [1, 0])
     line_b = Line([0, 0], [0, 1])
@@ -25,45 +25,45 @@
 end
 
 
-@testset "Line-plane intersection" begin
+@testset "Intersection of plane and line" begin
 
     origin = [0, 0, 0]
 
     line = Line(origin, [0, 0, 1])
     plane = Plane(origin, [0, 0, 1])
 
-    @test intersect(line, plane) == [0, 0, 0]
-    @test intersect(Line([3, 5, 2], [0, 0, 1]), plane) == [3, 5, 0]
+    @test intersect(plane, line) == [0, 0, 0]
+    @test intersect(plane, Line([3, 5, 2], [0, 0, 1])) == [3, 5, 0]
 
     message = "The line and plane are parallel."
-    @test_throws ArgumentError(message) intersect(Line(origin, [1, 0, 0]), plane)
+    @test_throws ArgumentError(message) intersect(plane, Line(origin, [1, 0, 0]))
 
     # The line is almost parallel to the plane.
     # This throws an error if the tolerance is large enough.
     line_almost_parallel = Line(zeros(3), [1, 0, 1e-2])
-    intersect(line_almost_parallel, plane)
-    @test_throws ArgumentError(message) intersect(line_almost_parallel, plane; atol=1e-2)
+    intersect(plane, line_almost_parallel)
+    @test_throws ArgumentError(message) intersect(plane, line_almost_parallel; atol=1e-2)
 end
 
 
+@testset "Intersection of circle/sphere and line" begin
 
-@testset "Line-circle intersection" begin
-
-    for (circle, line, point_expected_a, point_expected_b) in [
+    for (circle_or_sphere, line, point_expected_a, point_expected_b) in [
         (Circle([0, 0], 1), Line([0, 0], [1, 0]), [-1, 0], [1, 0]),
         (Circle([0, 0], 1), Line([0, 0], [0, 1]), [0, -1], [0, 1]),
         (Circle([0, 0], 1), Line([0, 1], [1, 0]), [0, 1], [0, 1]),
         (
-            Circle([0, 0], 1),
+            Circle([0.0, 0], 1),
             Line([0, 0.5], [1, 0]),
             [-√3 / 2, 0.5],
             [√3 / 2, 0.5],
         ),
         (Circle([1, 0], 1), Line([0, 0], [1, 0]), [0, 0], [2, 0]),
-        (Circle([1.5, 0], 1), Line([0, 0], [1, 0]), [0.5, 0], [2.5, 0]),
+        (Circle([1.5, 0], 1), Line([0.0, 0], [1, 0]), [0.5, 0], [2.5, 0]),
+        (Sphere([0, 0, 0], 1), Line([0, 0, 0], [1, 0, 0]), [-1, 0, 0], [1, 0, 0]),
     ]
 
-        point_a, point_b = intersect(line, circle)
+        point_a, point_b = intersect(circle_or_sphere, line)
 
         @test point_a == point_expected_a && point_b == point_expected_b
     end
