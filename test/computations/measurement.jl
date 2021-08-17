9b6e9f28a1cee3d8d59def732b7451f9852eb582
Andrew Hynes <andrewjhynes@gmail.com>
2021-08-17 23:42:29 +0200

    refactor: define measurement tests in loops

--- test/computations/measurement.jl
+++ test/computations/measurement.jl
@@ -1,17 +1,22 @@
 @testset "Cosine Similarity" begin
-    @test cosine_similarity([1, 0], [1, 0]) == 1
-    @test cosine_similarity([1, 0], [0, 1]) == 0
-    @test cosine_similarity([1, 0], [1, 1]) ≈ √2 / 2
-
-    @test cosine_similarity([1, 0, 0], [1, 0, 0]) == 1
-    @test cosine_similarity([1, 0, 0], [0, 0, 1]) == 0
-    @test cosine_similarity([1, 0, 0], [1, 1, 1]) ≈ √3 / 3
+    for (point_a, point_b, similarity_expected) in [
+        ([1, 0], [0, 1], 0),
+        ([1, 0], [1, 1], √2 / 2),
+        ([1, 0, 0], [1, 0, 0], 1),
+        ([1, 0, 0], [0, 0, 1], 0),
+        ([1, 0, 0], [1, 1, 1], √3 / 3),
+    ]
+        @test cosine_similarity(point_a, point_b) ≈ similarity_expected
+    end
 end
 
 @testset "Angle between vectors" begin
-    @test angle_between([1, 0], [1, 0]) == 0
-    @test angle_between([1, 0], [0, 1]) ≈ π / 2
-    @test angle_between([1, 0], [1, 1]) ≈ π / 4
-
-    @test angle_between([1, 0, 0], [1, 0, 1]) ≈ π / 4
+    for (point_a, point_b, angle_expected) in [
+        ([1, 0], [1, 0], 0),
+        ([1, 0], [0, 1], π / 2),
+        ([1, 0], [1, 1], π / 4),
+        ([1, 0, 0], [1, 0, 1], π / 4),
+    ]
+        @test angle_between(point_a, point_b) ≈ angle_expected
+    end
 end
