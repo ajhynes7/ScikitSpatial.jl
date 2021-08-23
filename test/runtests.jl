6d0fbf412689d41df9b2f93320cca2dcc5de8e04
Andrew Hynes <andrewjhynes@gmail.com>
2021-08-23 10:52:08 +0200

    test: run doctests as part of test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
+using Documenter
+using StaticArrays
 using Test
 
 using ScikitSpatial
-using StaticArrays
+
+
+DocMeta.setdocmeta!(ScikitSpatial, :DocTestSetup, :(using ScikitSpatial); recursive=true)
+doctest(ScikitSpatial)
 
 include("types/base.jl")
 include("computations/base.jl")
